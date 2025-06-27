08586b0ca3c40f3e3ede626694636cf5133ffd1d
Zoro07 <nadipallijaswanth541@gmail.com>
2025-06-27 21:11:23 +0530

    get purchased course content

--- backend/index.ts
+++ backend/index.ts
@@ -22,20 +22,20 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+    origin: "http://localhost:5173",
+    credentials: true
 }));
 
 app.use(errors);
 
 // routes
-app.use("/api/v1",userRoutes);
-app.use("/api/v1/course",courseRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1/course", courseRoutes);
 
-app.get("/test", (req: Request, res: Response , next: NextFunction) => {
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({
         success: true,
-        message:"API is Working"
+        message: "API is Working"
     });
 })
 
